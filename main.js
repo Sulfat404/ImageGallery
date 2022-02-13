0848e8a1599ef47cc21c2bf844428fa75d06abbd
Liza Prokopenko <elizaveta_prokopenko@mail.ru>
2022-02-13 18:22:01 +0300

    done: filter by word in input-placeholder, select-option, bigSizePhoto feach

--- main.js
+++ main.js
@@ -1,21 +1,94 @@
-async function getData() {
-    let res = await fetch('https://api.unsplash.com/photos/?client_id=PV3lCodTX9shtq5ZsjvQnAOH9qzO3GYk_ZC5f3K0zY0&per_page=12');
-    let data = await res.json();
-    console.log(data);
-    showData(data);
+getData("spring", "");
 
+// search
+const icon = document.querySelector('.placeholder__icon');
+const input = document.querySelector('.placeholder__search');
+
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+const btnClose = document.querySelector('.btn-close');
+
+const wrapper = document.querySelector('.wrapper');
+const galleryBigBlock = document.querySelector('.gallery-big-block');
+const bigSizePhoto = document.querySelector('.img-big-size-photo');
+
+const selectColor = document.querySelector('.select__color');
+
+let images = [];
+let currentImage = 0;
+
+// let searchQuery = document.querySelector('')
+
+async function getData(query, color) {
+    let url = `https://api.unsplash.com/search/photos?query=${query}&page=1&per_page=18&client_id=PV3lCodTX9shtq5ZsjvQnAOH9qzO3GYk_ZC5f3K0zY0`;
+    if (color && color !== "") {
+        url += `&color=${color}`;
+    }
+    let res = await fetch(url);
+    let data = await res.json();
+    showData(data);
 }
 
+function showKingSizePhoto(imgNum) {
+    wrapper.classList.add('blur-10');
+    galleryBigBlock.classList.remove('d-none');
+    bigSizePhoto.src = images[imgNum].urls.regular;
+    if (imgNum === 0){
+        btnPrev.classList.add('d-none');
+    } else {
+        btnPrev.classList.remove('d-none');
+    }
+    if (imgNum === (images.length - 1)){
+        btnNext.classList.add('d-none');
+    } else {
+        btnNext.classList.remove('d-none');
+    }
+    currentImage = imgNum;
+}
 
 function showData(data) {
     let gallery = document.querySelector('.gallery');
-    let key;
-    for(key in data){
-        const img = `<img class="gallery__img" src=${data[key].urls.regular} alt="image">`;
+    let imgs = document.querySelectorAll('.gallery__img');
+    for (let img of imgs){
+        img.remove();
+    }
+    
+    images = data.results;
+    for (let item of data.results) {
+        const img = `<img class="gallery__img" src=${item.urls.small} alt="image">`;
         gallery.insertAdjacentHTML('beforeend', img);
-        console.log(data[key].urls)
+    }
+    
+    let imgElements = document.querySelectorAll('.gallery__img');
+    for (let [i, imgElement] of imgElements.entries()) {
+        imgElement.addEventListener("click", () => showKingSizePhoto(i));
     }
 }
 
+function searchFilter() {
+    if(input.value !== '') {
+        input.blur();
+        getData(input.value, selectColor.value);
+    }
+}
 
-getData()
+icon.addEventListener('click', searchFilter);
+
+input.addEventListener('keydown', (e) => {
+    if(e.keyCode === 13) {
+        searchFilter();
+    }
+});
+
+btnPrev.addEventListener('click', () => {
+    showKingSizePhoto(currentImage - 1);
+});
+
+btnNext.addEventListener('click', () => {
+    showKingSizePhoto(currentImage + 1);
+});
+
+btnClose.addEventListener('click', () => {
+    wrapper.classList.remove('blur-10');
+    galleryBigBlock.classList.add('d-none');
+});
